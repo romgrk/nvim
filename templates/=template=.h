9d1599b5a520b6c7b41360decc6c506ba78c56d3
Rom Grk <romgrk.cc@gmail.com>
2021-07-31 21:50:12 -0400

    add .h template

---
+++ templates/=template=.h
@@ -0,0 +1,13 @@
+/*
+ * %FFILE%
+ * Copyright (C) %YEAR% %USER% <%MAIL%>
+ *
+ * Distributed under terms of the %LICENSE% license.
+ */
+
+#ifndef _%GUARD%_
+#define _%GUARD%_
+
+%HERE%
+
+#endif // _%GUARD%_
