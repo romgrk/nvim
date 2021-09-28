785179d13650f75077bd0e6d220c51bbbaa0bfbe
Rom Grk <romgrk.cc@gmail.com>
2021-09-27 23:28:00 -0400

    cleanup

---
+++ rc/plugins/context.after.lua
@@ -0,0 +1,15 @@
+#! /usr/bin/env lua
+--
+-- context.after.lua
+-- Copyright (C) 2021 romgrk <romgrk@dell-precision>
+--
+-- Distributed under terms of the MIT license.
+--
+
+-- require('treesitter-context').setup({
+--   patterns = {
+--     default = {
+--       'if',
+--     }
+--   }
+-- })
