4bf6a8223e531a9ad7ac00866ea3c02f6ce6a9b5
Rom Grk <romgrk.cc@gmail.com>
2022-07-10 19:03:08 -0400

    update

--- rc/plugins/sidebar.after.lua
+++ rc/plugins/sidebar.after.lua
@@ -3,24 +3,28 @@ local view = require('sidebar-nvim.view')
 local bufferline = require('bufferline.state')
 
 sidebar.setup({
-  open = false,
+  open = true,
   disable_default_keybindings = 0,
   side = 'left',
   initial_width = 35,
   hide_statusline = false,
   update_interval = 1000,
+  -- sections = {
+  --   'datetime',
+  --   'buffers',
+  --   'git',
+  --   'diagnostics',
+  --   'todos',
+  --   'files',
+  -- },
   sections = {
-    'datetime',
-    'buffers',
     'git',
     'diagnostics',
     'todos',
     'files',
   },
   section_separator = function(section, index)
-    -- local keys = table.concat(vim.tbl_keys(section.bindings or {}), ', ')
     return {
-      -- 'kb: ' .. keys,
       '',
       string.rep('─', view.View.width),
       '',
@@ -28,11 +32,12 @@ sidebar.setup({
   end,
   section_title_separator = {''},
   containers = {
-      attach_shell = '/bin/sh', show_all = true, interval = 5000,
+    attach_shell = '/bin/sh', show_all = true, interval = 5000,
   },
   todos = { ignored_paths = { '~' } },
   bindings = {
     ['q'] = function()
+      bufferline.set_offset(0)
       sidebar.close()
     end,
   },
